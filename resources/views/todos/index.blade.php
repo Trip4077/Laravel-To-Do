d1c8f9e20bd39290bb1e97c5a582b87bb7eb1e4c
Trip4077 <bj2336@email.vccs.edu>
2020-05-16 14:03:56 -0400

    initial

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title')
+    Todo List
+@endsection
+
+@section('content')
+    <h1 class="text-center m-5">Todos</h1>
+    
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Todos</div>
+
+
+                <div class="card-body">
+                    <ul class="list-group">
+                        @foreach($todos as $todo)
+                
+                            <li class="list-group-item">
+                                {{ $todo->name }}
+
+                                <a href="/todos/{{$todo->id}}" class="btn btn-primary btn-sm float-right">
+                                    view
+                                </a>
+                            </li>
+                
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> 
+@endsection
